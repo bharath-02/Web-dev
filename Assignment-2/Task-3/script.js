7c65b61e3992df0066b2c225b1c9968647735d1c
bhara <bharathJ222000@gmail.com>
2020-09-05 11:21:47 +0530

    small update

---
+++ Assignment-2/Task-3/script.js
@@ -0,0 +1,8 @@
+console.log(typeof(1));     //Number
+console.log(typeof(1.1));   //Number
+console.log(typeof("1.1")); //String
+console.log(typeof(true));  //Boolean
+console.log(typeof(null));  //Object
+console.log(typeof(undefined));//undefined
+console.log(typeof([]));    //Object
+console.log(typeof({}));    //Object
