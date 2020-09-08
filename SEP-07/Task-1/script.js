6c355a58a32f0b8a1388b394b3c1ed4b2c4ff731
bhara <bharathJ222000@gmail.com>
2020-09-08 15:19:55 +0530

    smaller update

--- SEP-07/Task-1/script.js
+++ SEP-07/Task-1/script.js
@@ -1,24 +1,7 @@
-/*var request=new XMLHttpRequest();
+var request=new XMLHttpRequest();
 request.open('GET','https://dog.ceo/api/breeds/list/all',true);
 request.onload=function() {
 	var data=JSON.parse(this.response);
 	console.log(data);
 }
-request.send();*/
-function f(a) {
-	b = a + 10
-	console.log(a);
-	return 20;
-}
-
-function f1(w) {
-	var dd = w(10)
-	console.log(dd);
-	return function(){console.log("check")}
-}
-var a11 = function(bb){return bb+10; } //10
-
-var ff = f(10);
-console.log(ff);
-var xx = f1(a11)
-xx();
+request.send();
