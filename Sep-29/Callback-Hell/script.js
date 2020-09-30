f106167dd8bbc06af6fdb2d86dbbc550f855a768
bhara <bharathJ222000@gmail.com>
2020-09-30 10:25:10 +0530

    countdown

--- Sep-29/Callback-Hell/script.js
+++ Sep-29/Callback-Hell/script.js
@@ -1,27 +1,60 @@
 
-var Timer; 
-var TotalSeconds;
+// var Timer; 
+// var TotalSeconds;
 
-function CreateTimer(id) { 
+// function CreateTimer(id) { 
     
-    Timer = document.getElementById(id); 
-    TotalSeconds = 10; 
-    Update(); 
-    window.setTimeout(change, 1000); 
-}
+//     Timer = document.getElementById(id); 
+//     TotalSeconds = 10; 
+//     Update(); 
+//     window.setTimeout(change, 1000); 
+// }
 
-function change() {
-        if (TotalSeconds <= 0) {
-            Timer.innerHTML='Happy New Year'; 
-            return; 
-        } 
-        TotalSeconds -= 1; 
-        Update();
-        window.setTimeout(change, 1000); 
-}
+// function change() {
+//         if (TotalSeconds <= 0) {
+//             Timer.innerHTML='Happy New Year'; 
+//             return; 
+//         } 
+//         TotalSeconds -= 1; 
+//         Update();
+//         window.setTimeout(change, 1000); 
+// }
 
-function Update() { 
-    Timer.innerHTML = TotalSeconds; 
-}
+// function Update() { 
+//     Timer.innerHTML = TotalSeconds; 
+// }
 
-CreateTimer('countdown');
+// CreateTimer('countdown');
+setTimeout(()=>{
+    document.getElementById('countdown').innerHTML = 10;
+    setTimeout(()=>{
+        document.getElementById('countdown').innerHTML = 9;
+        setTimeout(()=>{
+            document.getElementById('countdown').innerHTML = 8;
+            setTimeout(()=>{
+                document.getElementById('countdown').innerHTML = 7;
+                setTimeout(()=>{
+                    document.getElementById('countdown').innerHTML = 6;
+                    setTimeout(()=>{
+                        document.getElementById('countdown').innerHTML = 5;
+                        setTimeout(()=>{
+                            document.getElementById('countdown').innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById('countdown').innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById('countdown').innerHTML = 2;
+                                    setTimeout(()=>{
+                                        document.getElementById('countdown').innerHTML = 1;
+                                        setTimeout(()=>{
+                                            document.getElementById('countdown').innerHTML = 'Happy New Year';
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},0)
