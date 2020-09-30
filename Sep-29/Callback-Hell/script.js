8114505ad03cc247cd30b4293021f562068bf4e5
bhara <bharathJ222000@gmail.com>
2020-09-30 10:26:30 +0530

    countdown

--- Sep-29/Callback-Hell/script.js
+++ Sep-29/Callback-Hell/script.js
@@ -1,30 +1,4 @@
 
-// var Timer; 
-// var TotalSeconds;
-
-// function CreateTimer(id) { 
-    
-//     Timer = document.getElementById(id); 
-//     TotalSeconds = 10; 
-//     Update(); 
-//     window.setTimeout(change, 1000); 
-// }
-
-// function change() {
-//         if (TotalSeconds <= 0) {
-//             Timer.innerHTML='Happy New Year'; 
-//             return; 
-//         } 
-//         TotalSeconds -= 1; 
-//         Update();
-//         window.setTimeout(change, 1000); 
-// }
-
-// function Update() { 
-//     Timer.innerHTML = TotalSeconds; 
-// }
-
-// CreateTimer('countdown');
 setTimeout(()=>{
     document.getElementById('countdown').innerHTML = 10;
     setTimeout(()=>{
