da913a7f8b0eaf3758448184a669df88525ee240
bhara <bharathJ222000@gmail.com>
2020-10-23 10:39:18 +0530

    pet

--- Oct-07/Pet-shop/script.js
+++ Oct-07/Pet-shop/script.js
@@ -3,3 +3,79 @@ var Pet = /** @class */ (function () {
     }
     return Pet;
 }());
+// ------ Pet availability class -------
+var PetsAvailability = /** @class */ (function () {
+    function PetsAvailability(availablePets) {
+        var _a;
+        this.pets = new Array();
+        (_a = this.pets).push.apply(_a, availablePets);
+    }
+    PetsAvailability.prototype.insert = function (pet) {
+        this.pets.push(pet);
+    };
+    PetsAvailability.prototype.showPets = function () {
+        console.log(this.pets);
+    };
+    PetsAvailability.prototype.checkRequests = function (requestArray) {
+        for (var i = 0; i < requestArray.length; i++) {
+            var available = void 0;
+            for (var j = 0; j < this.pets.length; j++) {
+                if (requestArray[i].type === this.pets[j].type) {
+                    available = this.pets[j].availableCount;
+                    break;
+                }
+            }
+            if (!available) {
+                console.log(requestArray[i].type + " is not available");
+            }
+            else {
+                console.log(requestArray[i].type + " is available and the available count is " + available);
+            }
+        }
+    };
+    return PetsAvailability;
+}());
+var availablePets = new PetsAvailability([
+    { type: "Dog", availableCount: 2, age: 2 },
+    { type: "Cat", availableCount: 3, age: 2 },
+    { type: "Rabbit", availableCount: 2, age: 3 },
+    { type: "Love Birds", availableCount: 4, age: 2 },
+]);
+// ----- Pet Requests class ----------------
+var PetRequests = /** @class */ (function () {
+    function PetRequests(initialRequests) {
+        var _a;
+        this.requests = new Array();
+        (_a = this.requests).push.apply(_a, initialRequests);
+    }
+    PetRequests.prototype.addRequest = function (newRequest) {
+        this.requests.push(newRequest);
+    };
+    PetRequests.prototype.showRequests = function () {
+        console.log(this.requests);
+    };
+    PetRequests.prototype.checkAvailability = function () {
+        availablePets.checkRequests(this.requests);
+    };
+    return PetRequests;
+}());
+var request1 = new PetRequests([
+    {
+        type: "Duck",
+        count: 3
+    },
+    {
+        type: "Cat",
+        count: 2
+    },
+    {
+        type: "Mouse",
+        count: 1
+    },
+    {
+        type: 'Dog',
+        count: 2
+    },
+]);
+request1.addRequest({ type: "Fish", count: 2 });
+request1.checkAvailability();
--- Oct-07/Pet-shop/script.ts
+++ Oct-07/Pet-shop/script.ts
@@ -1,3 +1,99 @@
 class Pet {
-    
+    type: String;
+    availableCount: Number;
+    age: Number;
 }
+
+interface petDetails {
+    type: String;
+    count: Number;
+}
+
+// ------ Pet availability class -------
+
+class PetsAvailability {
+    pets: Array<Pet> = new Array();
+
+    constructor(availablePets: Array<Pet>) {
+        this.pets.push(...availablePets);
+    }
+
+    insert(pet: Pet) {
+        this.pets.push(pet);
+    }
+
+    showPets() {
+        console.log(this.pets);
+    }
+
+    checkRequests(requestArray: Array<petDetails>) {
+        for (let i = 0; i < requestArray.length; i++) {
+            let available;
+            for (let j = 0; j < this.pets.length; j++) {
+                if (requestArray[i].type === this.pets[j].type) {
+                    available = this.pets[j].availableCount;
+                    break;
+                }
+            }
+            if (!available) {
+                console.log(`${requestArray[i].type} is not available`);
+            } else {
+                console.log(
+                    `${requestArray[i].type} is available and the available count is ${available}`
+                );
+            }
+        }
+    }
+}
+
+let availablePets = new PetsAvailability([
+    { type: "Dog", availableCount: 2, age: 2 },
+    { type: "Cat", availableCount: 3, age: 2 },
+    { type: "Rabbit", availableCount: 2, age: 3 },
+    { type: "Love Birds", availableCount: 4, age: 2 },
+]);
+
+// ----- Pet Requests class ----------------
+
+class PetRequests {
+    requests: Array<petDetails> = new Array();
+
+    constructor(initialRequests: Array<petDetails>) {
+        this.requests.push(...initialRequests);
+    }
+
+    addRequest(newRequest: petDetails) {
+        this.requests.push(newRequest);
+    }
+
+    showRequests() {
+        console.log(this.requests);
+    }
+
+    checkAvailability() {
+        availablePets.checkRequests(this.requests);
+    }
+}
+
+let request1 = new PetRequests([
+    {
+        type: "Duck",
+        count: 3,
+    },
+    {
+        type: "Cat",
+        count: 2,
+    },
+    {
+        type: "Mouse",
+        count: 1,
+    },
+    {
+        type: 'Dog',
+        count: 2,
+    },
+]);
+
+request1.addRequest({ type: "Fish", count: 2 });
+
+request1.checkAvailability();
