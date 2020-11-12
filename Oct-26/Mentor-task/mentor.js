9c19ef9f2ae4722f193fe915d764c84089fa7071
bhara <bharathJ222000@gmail.com>
2020-11-12 15:50:13 +0530

    basic setup

---
+++ Oct-26/Mentor-task/mentor.js
@@ -0,0 +1,31 @@
+let mentors = [{
+        id: 1,
+        name: "Mentor-1",
+        email: "mentor1@gmail.com",
+        subject: "Tamil",
+        company: "GOOGLE"
+    },
+    {
+        id: 2,
+        name: "Mentor-2",
+        email: "mentor2@gmail.com",
+        subject: "English",
+        company: "AMAZON"
+    },
+    {
+        id: 3,
+        name: "Mentor-3",
+        email: "mentor3@gmail.com",
+        subject: "Maths",
+        company: "ZOHO"
+    },
+    {
+        id: 4,
+        name: "Mentor-4",
+        email: "mentor4@gmail.com",
+        subject: "Science",
+        company: "PAYPAL"
+    }
+]
+
+module.exports = mentors;
