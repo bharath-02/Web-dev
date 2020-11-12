67c2000646d891d6b13b4ef2ee11834fb41776ee
bhara <bharathJ222000@gmail.com>
2020-11-12 15:37:45 +0530

    basic setup

---
+++ Oct-26/Mentor-task/student.js
@@ -0,0 +1,27 @@
+let students = [{
+        id: 1,
+        name: 'Student-1',
+        subject: 'Tamil',
+        mentor: "Mentor-1"
+    },
+    {
+        id: 2,
+        name: 'Student-2',
+        subject: 'English',
+        mentor: "Mentor-2"
+    },
+    {
+        id: 3,
+        name: 'Student-3',
+        subject: 'Maths',
+        mentor: "Mentor-3"
+    },
+    {
+        id: 4,
+        name: 'Student-4',
+        subject: 'Science',
+        mentor: "Mentor-4"
+    }
+]
+
+module.exports = students;
