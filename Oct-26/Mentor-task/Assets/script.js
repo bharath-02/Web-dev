e6e1372e99715d2c464642027e597a006e6147a9
bhara <bharathJ222000@gmail.com>
2020-11-13 08:59:43 +0530

    added

--- Oct-26/Mentor-task/Assets/script.js
+++ Oct-26/Mentor-task/Assets/script.js
@@ -28,3 +28,66 @@ async function load() {
 }
 
 load();
+
+async function addMentor() {
+    document.getElementById('mentorForm').style.display = "none";
+
+    let data = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        company: document.getElementById('company').value
+    };
+
+    await fetch('https://localhost:3300/', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'content-Type': "application/json"
+        }
+    });
+}
+
+async function student(mentorid) {
+    document.getElementById('studentForm').style.display = 'block';
+    let mentor = mentordata.find((mentor) => mentor.id === parseInt(mentorid));
+    document.getElementById('mentor').value = mentor.name;
+    document.getElementById('subjectStudent').value = mentor.subject;
+}
+
+async function addStudent() {
+    let data = {
+        name: document.getElementById('studentName').value,
+        subject: document.getElementById('subjectStudent').value,
+        mentor: document.getElementById('mentorName').value
+    };
+    await fetch('https://localhost:3300/students', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'content-Type': 'application/json'
+        }
+    });
+
+    document.getElementById('studentForm').style.display = 'hidden';
+}
+
+async function getStudent(name) {
+    let rawdata = await fetch(`https://localhost:3300/students/${name}`);
+    let studentdata = await rawdata.json();
+
+    let ul = document.getElementById('students');
+    ul.innerHTML = '';
+
+    studentdata.forEach((student) => {
+        let li = document.createElement('li');
+        li.innerHTML = student.name;
+        ul.append(li);
+    });
+
+    document.getElementById('studentDetails').style.display = (document.getElementById('studentDetails').style.display === 'block') ? 'none' : 'block';
+}
+
+function mentorform() {
+    document.getElementById('mentorForm').style.display = 'block';
+}
