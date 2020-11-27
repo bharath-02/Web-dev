9fee65536f17775190085cb2c937018ff9cb7f92
bhara <bharathJ222000@gmail.com>
2020-11-27 19:42:10 +0530

    added

--- SEP-07/Task-1(Dog API)/script.js
+++ SEP-07/Task-1(Dog API)/script.js
@@ -1,7 +1,9 @@
 var request=new XMLHttpRequest();
-request.open('GET','https://dog.ceo/api/breeds/list/all',true);
+request.open('GET','https://www.googleapis.com/gmail/v1/users',true);
 request.onload=function() {
 	var data=JSON.parse(this.response);
 	console.log(data);
 }
 request.send();
+
+// https://dog.ceo/api/breeds/list/all
--- SEP-07/Task-2(CORS-Error)/script.js
+++ SEP-07/Task-2(CORS-Error)/script.js
@@ -1,7 +1,8 @@
 var request=new XMLHttpRequest();
-var url_string='https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
+// var url_string='https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
+var url_string='https://googleapis.com/gmail/v1/users';
 var proxy_string='https://cors-anywhere.herokuapp.com/';
-request.open('GET',proxy_string+url_string,true);
+request.open('GET',proxy_string+' '+url_string,true);
 request.onload=function() {
 	var data=JSON.parse(this.response);
 	console.log(data);
